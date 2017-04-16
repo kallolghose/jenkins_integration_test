26c8fc84fb11a07bed791472b8800f16ec140250
kallolghose <ghosekallol3@gmail.com>
2017-04-17 01:45:57 +0530

    second

--- src/com/jenkins/HelloWorld.java
+++ src/com/jenkins/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld
 {
 	public static void main(String [] args)
 	{
+		for(int i=0;i<args.length;i++)
+		{
+			System.out.println(args[i]);
+		}
 		System.out.println("Hello World");
 	}
 }
